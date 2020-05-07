be2e23396c24994aabb945d5f796b52783cdb691
davidk <kdavid@gmail.com>
2020-05-06 19:38:51 -0700

    Choose between discord-ptb (canary) and release discord on startup

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Allow access to the bus for notifications (libnotify)
 chown 1000:1000 -R /var/run/user/1000
-su discord -c /usr/bin/discord
+[[ -e "/usr/bin/discord" ]] && su discord -c /usr/bin/discord || su discord -c /usr/bin/discord-ptb
